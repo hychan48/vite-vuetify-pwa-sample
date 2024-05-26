ffc315d99d77caab4d70e004127c2bada7c084c1
JC <jchan48@gmail.com>
2024-05-26 01:12:44 -0400

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vite-vuetify-pwa-sample/sw.js', { scope: '/vite-vuetify-pwa-sample/' })})}
